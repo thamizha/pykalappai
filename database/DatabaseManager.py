3db3ce9cc5f98dac62ecec1bf50292ab2ec215b4
mohanrexd <mohanrd@visualbi.com>
2020-07-13 00:21:06 +0530

    Reboot initial error fixed

--- database/DatabaseManager.py
+++ database/DatabaseManager.py
@@ -1,76 +1,85 @@
 from database.Tables import *
+import os
 
 
 class DatabaseManager:
     def __init__(self):
         self.db = database
-        self.init_data = [
-                {'name': 'CTRL', 'parent': '0'},
-                {'name': 'ALT', 'parent': '0'},
-                {'name': 'NONE', 'parent': '0'},
-                {'name': '1', 'parent': '1'},
-                {'name': '2', 'parent': '1'},
-                {'name': '3', 'parent': '1'},
-                {'name': '4', 'parent': '1'},
-                {'name': '5', 'parent': '1'},
-                {'name': '6', 'parent': '1'},
-                {'name': '7', 'parent': '1'},
-                {'name': '8', 'parent': '1'},
-                {'name': '9', 'parent': '1'},
-                {'name': '0', 'parent': '1'},
-                {'name': '1', 'parent': '2'},
-                {'name': '2', 'parent': '2'},
-                {'name': '3', 'parent': '2'},
-                {'name': '4', 'parent': '2'},
-                {'name': '5', 'parent': '2'},
-                {'name': '6', 'parent': '2'},
-                {'name': '7', 'parent': '2'},
-                {'name': '8', 'parent': '2'},
-                {'name': '9', 'parent': '2'},
-                {'name': '0', 'parent': '2'},
-                {'name': 'F1', 'parent': '1'},
-                {'name': 'F2', 'parent': '1'},
-                {'name': 'F3', 'parent': '1'},
-                {'name': 'F4', 'parent': '1'},
-                {'name': 'F5', 'parent': '1'},
-                {'name': 'F6', 'parent': '1'},
-                {'name': 'F7', 'parent': '1'},
-                {'name': 'F8', 'parent': '1'},
-                {'name': 'F9', 'parent': '1'},
-                {'name': 'F10', 'parent': '1'},
-                {'name': 'F11', 'parent': '1'},
-                {'name': 'F12', 'parent': '1'},
-                {'name': 'F1', 'parent': '2'},
-                {'name': 'F2', 'parent': '2'},
-                {'name': 'F3', 'parent': '2'},
-                {'name': 'F4', 'parent': '2'},
-                {'name': 'F5', 'parent': '2'},
-                {'name': 'F6', 'parent': '2'},
-                {'name': 'F7', 'parent': '2'},
-                {'name': 'F8', 'parent': '2'},
-                {'name': 'F9', 'parent': '2'},
-                {'name': 'F10', 'parent': '2'},
-                {'name': 'F11', 'parent': '2'},
-                {'name': 'F12', 'parent': '2'},
-                {'name': 'F1', 'parent': '3'},
-                {'name': 'F2', 'parent': '3'},
-                {'name': 'F3', 'parent': '3'},
-                {'name': 'F4', 'parent': '3'},
-                {'name': 'F5', 'parent': '3'},
-                {'name': 'F6', 'parent': '3'},
-                {'name': 'F7', 'parent': '3'},
-                {'name': 'F8', 'parent': '3'},
-                {'name': 'F9', 'parent': '3'},
-                {'name': 'F10', 'parent': '3'},
-                {'name': 'F11', 'parent': '3'},
-                {'name': 'F12', 'parent': '3'},
+        self.init_data_language = [
+            {'language_name': 'Bamini - Tamil', 'file_path':'Tamil-bamini.txt.in', 'is_default': 10},
+            {'language_name': 'Inscript - Tamil', 'file_path':'Tamil-inscript.txt.in', 'is_default': 10},
+            {'language_name': 'Phonetic - Tamil', 'file_path':'Tamil-phonetic.txt.in', 'is_default': 10},
+            {'language_name': 'Tamil99 - Tamil', 'file_path':'Tamil-tamil99.txt.in', 'is_default': 10},
+            {'language_name': 'Typewriter - Tamil', 'file_path':'Tamil-typewriter.txt.in', 'is_default': 10}
+        ]
+        self.init_data_shortcut = [
+                {'name': 'CTRL', 'parent_id': '0'},
+                {'name': 'ALT', 'parent_id': '0'},
+                {'name': 'NONE', 'parent_id': '0'},
+                {'name': '1', 'parent_id': '1'},
+                {'name': '2', 'parent_id': '1'},
+                {'name': '3', 'parent_id': '1'},
+                {'name': '4', 'parent_id': '1'},
+                {'name': '5', 'parent_id': '1'},
+                {'name': '6', 'parent_id': '1'},
+                {'name': '7', 'parent_id': '1'},
+                {'name': '8', 'parent_id': '1'},
+                {'name': '9', 'parent_id': '1'},
+                {'name': '0', 'parent_id': '1'},
+                {'name': '1', 'parent_id': '2'},
+                {'name': '2', 'parent_id': '2'},
+                {'name': '3', 'parent_id': '2'},
+                {'name': '4', 'parent_id': '2'},
+                {'name': '5', 'parent_id': '2'},
+                {'name': '6', 'parent_id': '2'},
+                {'name': '7', 'parent_id': '2'},
+                {'name': '8', 'parent_id': '2'},
+                {'name': '9', 'parent_id': '2'},
+                {'name': '0', 'parent_id': '2'},
+                {'name': 'F1', 'parent_id': '1'},
+                {'name': 'F2', 'parent_id': '1'},
+                {'name': 'F3', 'parent_id': '1'},
+                {'name': 'F4', 'parent_id': '1'},
+                {'name': 'F5', 'parent_id': '1'},
+                {'name': 'F6', 'parent_id': '1'},
+                {'name': 'F7', 'parent_id': '1'},
+                {'name': 'F8', 'parent_id': '1'},
+                {'name': 'F9', 'parent_id': '1'},
+                {'name': 'F10', 'parent_id': '1'},
+                {'name': 'F11', 'parent_id': '1'},
+                {'name': 'F12', 'parent_id': '1'},
+                {'name': 'F1', 'parent_id': '2'},
+                {'name': 'F2', 'parent_id': '2'},
+                {'name': 'F3', 'parent_id': '2'},
+                {'name': 'F4', 'parent_id': '2'},
+                {'name': 'F5', 'parent_id': '2'},
+                {'name': 'F6', 'parent_id': '2'},
+                {'name': 'F7', 'parent_id': '2'},
+                {'name': 'F8', 'parent_id': '2'},
+                {'name': 'F9', 'parent_id': '2'},
+                {'name': 'F10', 'parent_id': '2'},
+                {'name': 'F11', 'parent_id': '2'},
+                {'name': 'F12', 'parent_id': '2'},
+                {'name': 'F1', 'parent_id': '3'},
+                {'name': 'F2', 'parent_id': '3'},
+                {'name': 'F3', 'parent_id': '3'},
+                {'name': 'F4', 'parent_id': '3'},
+                {'name': 'F5', 'parent_id': '3'},
+                {'name': 'F6', 'parent_id': '3'},
+                {'name': 'F7', 'parent_id': '3'},
+                {'name': 'F8', 'parent_id': '3'},
+                {'name': 'F9', 'parent_id': '3'},
+                {'name': 'F10', 'parent_id': '3'},
+                {'name': 'F11', 'parent_id': '3'},
+                {'name': 'F12', 'parent_id': '3'},
             ]
         try:
-            initialize()
+            self.db = initialize()
             self.init_tables()
-        except OperationalError:
+        except OperationalError as error:
             pass
-        self.db.connect()
+        if not self.db:
+            self.db.connect()
 
     @staticmethod
     def create_setting(values):
@@ -147,11 +156,15 @@ class DatabaseManager:
 
     @staticmethod
     def get_keyboard_path(keyboard_id):
-        return LanguageSetting.get(LanguageSetting.id == keyboard_id).file_path
+        language_setting = LanguageSetting.get(LanguageSetting.id == keyboard_id)
+        if language_setting.is_default == 10:
+            return os.getcwd() + "\\tables\\" + language_setting.file_path
+        return language_setting.file_path
 
     def init_tables(self):
         with self.db.atomic():
-            ShortcutKey.insert_many(self.init_data).execute()
+            ShortcutKey.insert_many(self.init_data_shortcut).execute()
+            LanguageSetting.insert_many(self.init_data_language).execute()
 
     def __del__(self):
         self.db.close()
