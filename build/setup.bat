abbd805fed8504127a800d1fb18a166c087791d8
mohanrexd <mohanrd@visualbi.com>
2020-07-19 19:39:26 +0530

    PywinHook dependency error fixed

--- build/setup.bat
+++ build/setup.bat
@@ -3,3 +3,4 @@ echo "Upgrading PIP"
 python -m pip install --upgrade pip
 echo "Installing Python dependencies"
 pip install -r requirements.txt
+pip install build\dependencies\pyWinhook-1.6.2-cp36-cp36m-win_amd64.whl
